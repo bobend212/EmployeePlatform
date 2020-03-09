2f2250cc777f2c50708ffb4b206499b6f818898a
MateuszK <mateusz.konopka11@gmail.com>
2020-03-09 21:41:32 +0100

    start extending API

--- EmpPlatform-API/Models/User.cs
+++ EmpPlatform-API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace EmpPlatform_API.Models
 {
     public class User
@@ -6,5 +9,11 @@ namespace EmpPlatform_API.Models
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public ICollection<Project> Projects { get; set; }
+
     }
 }
