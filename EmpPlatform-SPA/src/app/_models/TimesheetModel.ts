1dd023843f1a9f2f9264a0b702827c8fbaefbbe1
MateuszK <mateusz.konopka11@gmail.com>
2020-04-05 12:32:57 +0200

    continue prev commit

---
+++ EmpPlatform-SPA/src/app/_models/TimesheetModel.ts
@@ -0,0 +1,11 @@
+export interface TimesheetModel {
+    project: string;
+    workType: string
+    monday: string;
+    tuesday: string;
+    wednesday: string;
+    thursday: string;
+    friday: string;
+    saturday: string;
+    sunday: string;
+}
--- EmpPlatform-SPA/src/app/timesheets/mytimesheet/mytimesheet.component.ts
+++ EmpPlatform-SPA/src/app/timesheets/mytimesheet/mytimesheet.component.ts
@@ -4,6 +4,9 @@ import { AuthService } from 'src/app/_services/auth.service';
 import { Timesheet } from 'src/app/_models/Timesheet';
 import { ActivatedRoute } from '@angular/router';
 import { BsLocaleService, BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { TimesheetModel } from 'src/app/_models/TimesheetModel';
+import { WorkType } from 'src/app/_models/WorkType';
+import { Project } from 'src/app/_models/Project';
 
 @Component({
   selector: 'app-mytimesheet',
@@ -21,6 +24,11 @@ export class MytimesheetComponent implements OnInit {
 
   bsConfig: Partial<BsDatepickerConfig>;
 
+  dynamicArray: Array<TimesheetModel> = [];
+  newDynamic: any = {};
+  projectList: Project[];
+  workTypesList: WorkType[];
+
   constructor(private timesheetService: TimesheetService, private authService: AuthService, private route: ActivatedRoute, private localeService: BsLocaleService) { }
 
   ngOnInit() {
@@ -32,13 +40,55 @@ export class MytimesheetComponent implements OnInit {
 
     //this.startDate.setDate(this.bsValue.getDate());
     //this.endDate.setDate(this.startDate.getDate() + 7);
+    this.newDynamic = { monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "", sunday: "" };
+    this.dynamicArray.push(this.newDynamic);
+
+    this.loadProjects();
+    this.loadWorkTypes();
   }
 
   getMonday(date: Date, dayOfWeek: number) {
     let diff = (7 + (date.getDay() - dayOfWeek)) % 7;
   }
 
+  addRow(index) {
+    this.newDynamic = { monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "", sunday: "" };
+    this.dynamicArray.push(this.newDynamic);
+    return true;
+  }
 
+  deleteRow(index) {
+    if (this.dynamicArray.length == 1) {
+      return false;
+    } else {
+      this.dynamicArray.splice(index, 1);
+      return true;
+    }
+  }
+
+  loadWorkTypes() {
+    this.timesheetService.getWorkTypesList().subscribe(
+      (workTypes: WorkType[]) => {
+        this.workTypesList = workTypes;
+        console.log(workTypes);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadProjects() {
+    this.timesheetService.getProjectsList().subscribe(
+      (projects: Project[]) => {
+        this.projectList = projects;
+        console.log(projects);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 
 
 }
